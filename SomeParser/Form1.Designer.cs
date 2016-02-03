843c3966a22d20f8f5b837da9c9c8374e2b2b048
Sergey Shevchenko <shevchenko.serg.88@gmail.com>
2016-02-03 23:01:16 +0200

    CefSharp used

--- SomeParser/Form1.Designer.cs
+++ SomeParser/Form1.Designer.cs
@@ -28,12 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonDisplayDevTools = new System.Windows.Forms.Button();
+            this.buttonSourceCode = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(686, 371);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonDisplayDevTools
+            // 
+            this.buttonDisplayDevTools.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDisplayDevTools.Location = new System.Drawing.Point(704, 12);
+            this.buttonDisplayDevTools.Name = "buttonDisplayDevTools";
+            this.buttonDisplayDevTools.Size = new System.Drawing.Size(110, 30);
+            this.buttonDisplayDevTools.TabIndex = 1;
+            this.buttonDisplayDevTools.Text = "Display dev tools";
+            this.buttonDisplayDevTools.UseVisualStyleBackColor = true;
+            this.buttonDisplayDevTools.Click += new System.EventHandler(this.buttonDisplayDevTools_Click);
+            // 
+            // buttonSourceCode
+            // 
+            this.buttonSourceCode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSourceCode.Location = new System.Drawing.Point(704, 48);
+            this.buttonSourceCode.Name = "buttonSourceCode";
+            this.buttonSourceCode.Size = new System.Drawing.Size(110, 32);
+            this.buttonSourceCode.TabIndex = 2;
+            this.buttonSourceCode.Text = "View source code";
+            this.buttonSourceCode.UseVisualStyleBackColor = true;
+            this.buttonSourceCode.Click += new System.EventHandler(this.buttonSourceCode_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 395);
+            this.Controls.Add(this.buttonSourceCode);
+            this.Controls.Add(this.buttonDisplayDevTools);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button buttonDisplayDevTools;
+        private System.Windows.Forms.Button buttonSourceCode;
     }
 }
 
--- SomeParser/Form1.cs
+++ SomeParser/Form1.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CefSharp;
+using CefSharp.Internals;
+using CefSharp.WinForms;
 
 namespace SomeParser
 {
     public partial class Form1 : Form
     {
+        private ChromiumWebBrowser m_chromeBrowser;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Cef.Initialize();
+            m_chromeBrowser = new ChromiumWebBrowser("https://auto.ria.com/search/?category_id=1&marka_id=15&model_id=1082#category_id=1&s_yers[0]=2006&po_yers[0]=0&price_do=8000&currency=1&marka_id[0]=15&model_id[0]=1082&countpage=10&page=0&power_name=1&engineVolumeFrom=&engineVolumeTo=&raceTo=125&damage=1");
+            panel1.Controls.Add(m_chromeBrowser);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+
+        private void buttonDisplayDevTools_Click(object sender, EventArgs e)
+        {
+            m_chromeBrowser.ShowDevTools();
+        }
+
+        private void buttonSourceCode_Click(object sender, EventArgs e)
+        {
+            var t = m_chromeBrowser.GetSourceAsync();
+            t.Wait();
+            var result = t.Result;
+
+            m_chromeBrowser
         }
     }
 }
